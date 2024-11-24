d04f447d64439ba56ee96e61651c501a1894d3da
LevelUpInTech <72570971+LevelUpInTech@users.noreply.github.com>
2024-11-24 18:52:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0453ec754f44f9a4a"
   instance_type = "t3.nano"
-  subnet_id     = "subnet-0b465d667b85d6382"
+  subnet_id     = "your-subnet-ID"
 
   tags = {
     Name = "my-instance"
